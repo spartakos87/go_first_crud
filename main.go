d9fa58f94aaae15bdc0af3b061a2bf39124fb087
spartakos <serepasf@gmail.com>
2021-06-04 18:32:00 +0300

    add comment about psql

--- main.go
+++ main.go
@@ -7,5 +7,6 @@ import (
 func main() {
 	// This repository is based on this article https://tutorialedge.net/golang/creating-restful-api-with-golang/
 	//TODO save articles in Postgres https://www.enterprisedb.com/postgres-tutorials/postgresql-and-golang-tutorial
+	//article about go and postgres https://golangdocs.com/golang-postgresql-example
 	routes.HandleRequests()
 }
