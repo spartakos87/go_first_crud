9572614b70a1f7044b3c97ef5eeffcb8f3768a5a
spartakos <serepasf@gmail.com>
2021-06-04 18:30:45 +0300

    add comment

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"FirstRestGo/routes"
 )
 
-
 func main() {
+	// This repository is based on this article https://tutorialedge.net/golang/creating-restful-api-with-golang/
 	//TODO save articles in Postgres https://www.enterprisedb.com/postgres-tutorials/postgresql-and-golang-tutorial
 	routes.HandleRequests()
 }
