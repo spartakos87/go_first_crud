68b9dd61b62cef215c7f99b0d496b5fc23d8f656
spartakos <serepasf@gmail.com>
2021-06-11 14:14:43 +0300

    "read" claims from JWT

--- jwthandler/jwthandler.go
+++ jwthandler/jwthandler.go
@@ -1,6 +1,7 @@
 package jwthandler
 
 import (
+	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"time"
 )
@@ -24,12 +25,15 @@ func ValidateJWT(tokenString string) (string, error) {
 	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
 		return []byte("my_secret_key"), nil
 	})
+	// How we get the user from token!!!
+	tclaims, _ := token.Claims.(jwt.MapClaims)
+	fmt.Println(tclaims["user_id"])
+	//----------------------------------------
 	if err != nil {
 		return "", err
 	}
 	if token.Valid {
 		return "OK", nil
 	}
-
 	return "", nil
 }
